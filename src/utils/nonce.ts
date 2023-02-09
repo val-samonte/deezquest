6f458273646ebcd73679ead31064f52be50e959d
Val <val.samonte@gmail.com>
2023-02-10 00:09:23 +0800

    Request nonce endpoint

---
+++ src/utils/nonce.ts
@@ -0,0 +1,33 @@
+import { Keypair } from '@solana/web3.js'
+import { sign } from 'tweetnacl'
+import bs58 from 'bs58'
+
+const dappKey = Keypair.fromSecretKey(
+  new Uint8Array(bs58.decode(process.env.APP_KEYPAIR ?? '')),
+)
+
+export function createNonce() {
+  const nonce = Keypair.generate().publicKey.toBase58().substring(0, 12)
+  const time = new Date().getTime()
+  const data = nonce + '.' + time
+  const signature = sign(Buffer.from(data), dappKey.secretKey)
+  return data + '.' + bs58.encode(signature).substring(0, 12)
+}
+
+export function verifyNonce(token: string) {
+  const currentTime = new Date().getTime()
+
+  // token should have 3 parts
+  const part = token.split('.')
+  if (part.length !== 3) return false
+
+  // verify time expiry (should be less than 5 minutes old)
+  if (parseInt(part[1]) + 300_000 < currentTime) return false
+
+  // verify authenticity
+  const data = part[0] + '.' + part[1]
+  const signature = sign(Buffer.from(data), dappKey.secretKey)
+  if (part[2] !== bs58.encode(signature).substring(0, 12)) return false
+
+  return true
+}
