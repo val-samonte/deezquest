e65c0591a40349e18424224c6dd5c5ae63d42065
Val <val.samonte@gmail.com>
2023-02-10 15:52:18 +0800

    Render Pixi canvas

--- src/app/battle/Stage.tsx
+++ src/app/battle/Stage.tsx
@@ -1,10 +1,49 @@
 'use client'
 
+import { Application } from 'pixi.js'
+import { useLayoutEffect, useRef, useState } from 'react'
+import { Stage as PixiStage } from 'react-pixi-fiber'
+
 export default function Stage() {
+  const div = useRef<HTMLDivElement | null>(null)
+  const [app, setApp] = useState<Application | null>(null)
+
+  useLayoutEffect(() => {
+    if (!div.current) return
+
+    const canvas = document.createElement('canvas')
+    div.current.appendChild(canvas)
+
+    const app = new Application({
+      view: canvas,
+      width: div.current.clientWidth,
+      height: div.current.clientHeight,
+    })
+    setApp(app)
+
+    const resize = () => {
+      if (!div.current) return
+      app.renderer.resize(div.current.clientWidth, div.current.clientHeight)
+    }
+
+    window.addEventListener('resize', resize)
+
+    return () => {
+      window.removeEventListener('resize', resize)
+      setApp(null)
+      app.destroy(true, true)
+    }
+  }, [])
+
   return (
     <div className='w-full h-full flex portrait:flex-col'>
       <div className='flex-auto'></div>
-      <div className='landscape:h-full portrait:w-full aspect-square bg-black flex-none'></div>
+      <div
+        ref={div}
+        className='landscape:h-full portrait:w-full aspect-square bg-black flex-none'
+      >
+        {app && <PixiStage app={app} />}
+      </div>
       <div className='flex-auto'></div>
     </div>
   )
--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -1,11 +1,19 @@
-import Stage from './Stage'
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const Stage = dynamic(() => import('./Stage'), { ssr: false })
 
 export default function Battle() {
   return (
     <main className='flex flex-col landscape:justify-center w-full h-full'>
-      <div className='max-w-[100vw] max-h-[100vh] landscape:w-full landscape:aspect-[5/3] portrait:h-full portrait:aspect-[3/5] mx-auto'>
-        <Stage />
+      <div className='flex-auto flex items-center justify-center'>
+        <div className='max-w-[100vw] max-h-[calc(100vh-56px)] landscape:w-full landscape:aspect-[5/3] portrait:h-full portrait:aspect-[3/5] mx-auto bg-black/25'>
+          <Suspense fallback={null}>
+            <Stage />
+          </Suspense>
+        </div>
       </div>
+      <div className='flex-none h-14 w-full bg-black/50'></div>
     </main>
   )
 }
