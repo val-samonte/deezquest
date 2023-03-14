db3d3864c0f26f0e6322fa7680fa0aa447dc509b
Val <val.samonte@gmail.com>
2023-03-15 01:09:08 +0800

    More gam functions mirroring

--- anchor/programs/deezquest/src/states/game.rs
+++ anchor/programs/deezquest/src/states/game.rs
@@ -292,6 +292,7 @@ pub fn get_next_turn(
             } else if seq1[i] < seq2[i] {
                 return (hero2, pubkey2);
             }
+            i += 1;
         }
 
         // random using game hash
@@ -308,6 +309,7 @@ pub fn get_next_turn(
             } else if seq1[i] < seq2[i] {
                 return (hero2, pubkey2);
             }
+            i += 1;
         }
 
         panic!("Heroes are too identical, even having the same public key hash result.");
@@ -317,3 +319,309 @@ pub fn get_next_turn(
 
     (hero2, pubkey2)
 }
+
+pub fn hash_to_tiles(&hash: [u8; 32]) -> [Option<u8>; 64] {
+    let mut tiles: [Option<u8>; 64] = [None; 64];
+    let mut i = 0;
+    while i < 32 {
+        let byte = hash[i];
+
+        tiles[i] = (byte * 0xf) % 7;
+        tiles[i + 32] = ((byte >> 4) & 0xf) % 7;
+
+        i += 1;
+    }
+    tiles
+}
+
+pub fn has_match(&tiles: [Option<u8>; 64]) -> bool {
+    let mut i = 0;
+    while i < 64 {
+        let node_type = tiles[i];
+        let col = i % 8;
+        let row = i / 8;
+
+        // vertical
+        if row < 6 {
+            if !node_type.is_none() && node_type == tiles[i + 8] && node_type == tiles[i + 16] {
+                return true;
+            }
+        }
+
+        // horizontal
+        if col < 6 {
+            if !node_type.is_none() && node_type == tiles[i + 1] && node_type == tiles[i + 2] {
+                return true;
+            }
+        }
+
+        i += 1;
+    }
+
+    false
+}
+
+pub fn get_matches(&tiles: [Option<u8>; 64]) -> ([Option<u8>; 64], [u8; 7], [u8; 8]) {
+    let mut matches: [Option<u8>; 64] = [None; 64];
+    let mut count: [u8; 7] = [0; 7];
+    let mut depths: [u8; 8] = [0; 8];
+
+    let mut i = 0;
+    while i < 64 {
+        let node_type = tiles[i];
+        let col = i % 8;
+        let row = i / 8;
+
+        // vertical
+        if row < 6 {
+            if !node_type.is_none() && node_type == tiles[i + 8] && node_type == tiles[i + 16] {
+                let node = node_type.unwrap();
+                if matches[i]._is_none {
+                    depths[col] += 1;
+                    count[node] += 1;
+                    matches[i] = node_type;
+                }
+                if matches[i + 8]._is_none {
+                    depths[col] += 1;
+                    count[node] += 1;
+                    matches[i + 8] = node_type;
+                }
+                if matches[i + 16]._is_none {
+                    depths[col] += 1;
+                    count[node] += 1;
+                    matches[i + 16] = node_type;
+                }
+            }
+        }
+
+        // horizontal
+        if col < 6 {
+            if !node_type.is_none() && node_type == tiles[i + 1] && node_type == tiles[i + 2] {
+                let node = node_type.unwrap();
+                if matches[i]._is_none {
+                    depths[col] += 1;
+                    count[node] += 1;
+                    matches[i] = node_type;
+                }
+                if matches[i + 1]._is_none {
+                    depths[col + 1] += 1;
+                    count[node] += 1;
+                    matches[i + 1] = node_type;
+                }
+                if matches[i + 2]._is_none {
+                    depths[col + 2] += 1;
+                    count[node] += 1;
+                    matches[i + 2] = node_type;
+                }
+            }
+        }
+
+        i += 1;
+    }
+
+    (matches, depths, count)
+}
+
+pub fn substract(&tiles: [Option<u8>; 64], &mask: [Option<u8>; 64]) -> [Option<u8>; 64] {
+    let mut result: [Option<u8>; 64] = [None; 64];
+
+    let mut i = 0;
+    while i < 64 {
+        if mask[i].is_none() {
+            result[i] = tiles[i];
+        }
+        i += 1;
+    }
+
+    result
+}
+
+pub fn apply_gravity(&mut tiles: [Option<u8>; 64], depths: [u8; 8]) -> [Option<u8>; 64] {
+    let gravity_map: [Option<u8>; 64] = [None; 64];
+
+    let mut i = 0;
+    while i < 8 {
+        if depths[i] == 0 {
+            continue;
+        };
+
+        let mut gravity = 0;
+        let mut blanks: Vec<usize> = Vec::new();
+
+        let mut j = 7;
+        while j >= 0 {
+            let id: usize = j * 8 + i;
+
+            if tiles[id].is_none() {
+                gravity += 1;
+                blanks.push(id);
+                continue;
+            }
+
+            let mut k = 0;
+            while k < blanks.len() {
+                gravity_map[blanks[k]] = gravity
+                    + match gravity_map[blanks[k]] {
+                        Some(n) => n,
+                        None => 0,
+                    };
+
+                k += 1;
+            }
+
+            blanks = Vec::new();
+
+            let node_type = tiles[id];
+            let dest = (j + gravity) * 8 + i;
+            tiles[id] = None;
+            tiles[dest] = Some(node_type);
+            gravity_map[id] = Some(gravity);
+
+            j -= 1;
+        }
+
+        let mut k = 0;
+        while k < blanks.len() {
+            gravity_map[blanks[k]] = gravity
+                + match gravity_map[blanks[k]] {
+                    Some(n) => n,
+                    None => 0,
+                };
+
+            k += 1;
+        }
+
+        i += 1;
+    }
+
+    (gravity_map, tiles)
+}
+
+pub fn fill(
+    &tiles: [Option<u8>; 64],
+    &fillers: [Option<u8>; 64],
+    &depths: [u8; 8],
+) -> [Option<u8>; 64] {
+    let mut i = 0;
+    while i < 8 {
+        if depths[i] == 0 {
+            continue;
+        }
+        let mut j = 0;
+        while j < depths[i] {
+            let id = (j * 8) + i;
+            tiles[id] = fillers[id];
+            j += 1;
+        }
+        i += 1;
+    }
+    tiles
+}
+
+// export const skillCountPerMana = (mana: number[], cost: SkillCost) => {
+//     const costs = [cost.fire, cost.wind, cost.water, cost.earth]
+
+//     return mana.map((mp, i) => {
+//       if (typeof costs[i] === 'undefined') return undefined
+
+//       if (costs[i] === 0) {
+//         return mp >= 1 ? 1 : 0
+//       }
+
+//       return mp / costs[i]!
+//     })
+//   }
+
+//   export const isExecutable = (hero: Hero, cost: SkillCost) => {
+//     const count = skillCountPerMana(
+//       [hero.fireMp, hero.windMp, hero.watrMp, hero.eartMp],
+//       cost,
+//     )
+
+//     return count.every((n) => {
+//       if (typeof n === 'undefined') return true
+//       if (n >= 1) return true
+
+//       return false
+//     })
+//   }
+
+//   export const deductMana = (hero: Hero, cost: SkillCost) => {
+//     hero.fireMp -= cost.fire === 0 ? hero.fireMp : cost.fire ?? 0
+//     hero.windMp -= cost.wind === 0 ? hero.windMp : cost.wind ?? 0
+//     hero.watrMp -= cost.water === 0 ? hero.watrMp : cost.water ?? 0
+//     hero.eartMp -= cost.earth === 0 ? hero.eartMp : cost.earth ?? 0
+
+//     return hero
+//   }
+
+//   export const executableCommands = (
+//     originalHero: Hero,
+//     absorbedCommands: number[] /* [ATK, SUP, SPE] */,
+//   ) => {
+//     let hero = { ...originalHero } as Hero
+//     const flags = [false, false, false]
+//     const offensiveSkill = skills[hero.offensiveSkill]
+//     const supportiveSkill = skills[hero.supportiveSkill]
+//     const specialSkill = skills[hero.specialSkill]
+//     const queue: {
+//       hero: Hero
+//       lvl?: number
+//       skill?: Skill
+//       attack?: number
+//       armor?: number
+//     }[] = []
+
+//     // priority: special skill > supportive skill > offensive skill
+
+//     // Special Skill requires to have 4 or more amulets to be executed
+//     if (absorbedCommands[2] > 3) {
+//       if (isExecutable(hero, specialSkill.cost)) {
+//         hero = deductMana(hero, specialSkill.cost)
+//         queue.push({
+//           hero: { ...hero },
+//           skill: specialSkill,
+//         })
+//         flags[2] = true
+//       }
+//     }
+
+//     if (absorbedCommands[1] > 2) {
+//       if (isExecutable(hero, supportiveSkill.cost)) {
+//         hero = deductMana(hero, supportiveSkill.cost)
+//         queue.push({
+//           hero: { ...hero },
+//           lvl: absorbedCommands[1] > 4 ? 3 : absorbedCommands[1] === 4 ? 2 : 1,
+//           skill: supportiveSkill,
+//         })
+//         flags[1] = true
+//       } else {
+//         queue.push({
+//           armor: absorbedCommands[1],
+//           hero: { ...hero },
+//         })
+//       }
+//     }
+
+//     if (absorbedCommands[0] > 2) {
+//       if (isExecutable(hero, offensiveSkill.cost)) {
+//         hero = deductMana(hero, offensiveSkill.cost)
+//         queue.push({
+//           hero: { ...hero },
+//           lvl: absorbedCommands[0] > 4 ? 3 : absorbedCommands[0] === 4 ? 2 : 1,
+//           skill: offensiveSkill,
+//         })
+//         flags[0] = true
+//       } else {
+//         queue.push({
+//           attack: absorbedCommands[0],
+//           hero: { ...hero },
+//         })
+//       }
+//     }
+
+//     return {
+//       flags,
+//       queue,
+//     }
+//   }
