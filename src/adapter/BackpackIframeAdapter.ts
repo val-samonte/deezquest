c8031869de7d63e0a6d814f6935fb81dad7b4b54
Val <val.samonte@gmail.com>
2023-04-22 16:51:29 +0800

    Revert backpack origin

--- src/adapter/BackpackIframeAdapter.ts
+++ src/adapter/BackpackIframeAdapter.ts
@@ -9,7 +9,7 @@ import bs58 from 'bs58'
 const BACKPACK_ORIGIN =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:19006'
-    : 'https://arweave.net'
+    : 'https://embed.xnfts.dev'
 
 export const BackpackIframeWalletName =
   'Backpack Iframe' as WalletName<'Backpack Iframe'>
