23910dda6a9eaad5f43d40b4177bf763b41c92d2
Val <val.samonte@gmail.com>
2023-05-07 02:47:18 +0800

    BGM tracks (home, battle)

---
+++ src/app/BgmManager.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { useEffect, useRef } from 'react'
+import useSound from 'use-sound'
+
+export default function BgmManager() {
+  const pathname = usePathname()
+  const homeInit = useRef(false)
+  const battleInit = useRef(false)
+
+  const [playHome, { sound: homeSound }] = useSound('/bgm_home.ogg', {
+    volume: 0.5,
+    loop: true,
+    interrupt: true,
+  })
+
+  const [playBattle, { sound: battleSound }] = useSound('/bgm_battle.ogg', {
+    volume: 0.5,
+    loop: true,
+    interrupt: true,
+  })
+
+  useEffect(() => {
+    if (homeSound && !homeInit.current) {
+      homeSound.on('fade', () => {
+        if (homeSound.volume() === 0) {
+          homeSound.stop()
+          console.log('HOME SOUND STOP')
+        }
+      })
+      homeInit.current = true
+    }
+  }, [homeSound])
+
+  useEffect(() => {
+    if (battleSound && !battleInit.current) {
+      battleSound.on('fade', () => {
+        if (battleSound.volume() === 0) {
+          battleSound.stop()
+          console.log('BATTLE SOUND STOP')
+        }
+      })
+      battleInit.current = true
+    }
+  }, [battleSound])
+
+  useEffect(() => {
+    if (pathname && homeInit.current && battleInit.current) {
+      if (pathname.includes('/battle')) {
+        if (battleSound && !battleSound.playing()) {
+          playBattle()
+          battleSound.fade(battleSound.volume(), 0.5, 1000)
+        }
+        if (homeSound && homeSound.playing()) {
+          homeSound.fade(homeSound.volume(), 0, 1000)
+        }
+      } else {
+        if (homeSound && !homeSound.playing()) {
+          playHome()
+          homeSound.fade(homeSound.volume(), 0.5, 1000)
+        }
+        if (battleSound && battleSound.playing()) {
+          battleSound.fade(battleSound.volume(), 0, 1000)
+        }
+      }
+    }
+  }, [pathname, homeSound, battleSound, playHome, playBattle])
+
+  return null
+}
