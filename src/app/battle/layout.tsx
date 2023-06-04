020752fac7f53fe62443aed8023a2c25d93fbf5e
Val <val.samonte@gmail.com>
2023-06-04 22:50:29 +0800

    Battle UI WIP

--- src/app/battle/layout.tsx
+++ src/app/battle/layout.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import PageContainer from '@/components/PageContainer'
 import PageTitle from '@/components/PageTitle'
 import classNames from 'classnames'
 import Image from 'next/image'
+import { useMemo } from 'react'
+import useMeasure from 'react-use-measure'
 
 export default function BattleLayout() {
   return (
@@ -94,8 +98,12 @@ const mask =
   'linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 90%, rgba(0,0,0,0) 100%)'
 
 function HeroCard({ className, flip }: { className?: string; flip?: boolean }) {
+  const [ref, rect] = useMeasure()
+  const expand = useMemo(() => rect.width * 2 <= rect.height, [rect])
+
   return (
     <div
+      ref={ref}
       className={classNames(
         'w-full h-full flex flex-col',
         // 'bg-teal-700 portrait:xm:bg-green-500',
@@ -119,12 +127,12 @@ function HeroCard({ className, flip }: { className?: string; flip?: boolean }) {
           )}
         />
       </div>
-      <div className='flex-none w-full relative flex flex-col gap-2 pb-2 px-2'>
+      <div className='flex-none w-full relative flex flex-col gap-3 pb-3 px-2'>
         <HeroLifebar flip={flip} />
-        <div className='grid grid-cols-12 gap-2'>
-          <HeroSkill flip={flip} />
-          <HeroSkill flip={flip} />
-          <HeroSkill flip={flip} />
+        <div className={classNames('flex gap-1', expand && 'flex-col')}>
+          <HeroSkill flip={flip} expand={expand} />
+          <HeroSkill flip={flip} expand={expand} />
+          <HeroSkill flip={flip} expand={expand} />
         </div>
       </div>
     </div>
@@ -230,31 +238,90 @@ function HeroLifebar({ flip }: { flip?: boolean }) {
   )
 }
 
-function HeroSkill({ flip }: { flip?: boolean }) {
-  return (
-    <div
-      className={classNames(
-        'col-span-4 xs:col-span-12 portrait:xm:col-span-4',
-        'flex-none grid grid-cols-12',
-        'w-full',
-        'bg-pink-500',
-      )}
-    >
-      <div
-        className={classNames(
-          'w-full',
-          'aspect-[1/10] xs:aspect-auto portrait:xm:aspect-[1/5]',
-        )}
-      ></div>
-      {/* <div
-        className={classNames(
-          'aspect-square portrait:xm:h-full',
-          // 'portrait:xm:aspect-[3/1]',
-          'col-span-3 row-start-1 portrait:xm:col-span-2',
-          flip ? 'col-start-10 portrait:xm:col-start-11' : 'col-start-1',
-          'bg-purple-500',
-        )}
-      ></div> */}
-    </div>
-  )
+function HeroSkill({ flip, expand }: { flip?: boolean; expand?: boolean }) {
+  if (expand) {
+    return (
+      <div className='grid grid-cols-12 px-2'>
+        <div
+          className={classNames(
+            'flex items-center justify-center',
+            'relative col-span-3 row-start-1 aspect-square p-2',
+            // 'bg-pink-500',
+            flip && 'col-start-10',
+          )}
+        >
+          <div className='absolute inset-1 -rotate-45'>
+            <div
+              className={classNames(
+                'absolute top-0 left-0 w-3 h-3',
+                'border-t border-l border-amber-400/50',
+              )}
+            />
+            <div
+              className={classNames(
+                'absolute bottom-0 right-0 w-3 h-3',
+                'border-b border-r border-amber-400/50',
+              )}
+            />
+          </div>
+          <div
+            className={classNames(
+              // 'opacity-0',
+              'w-full h-full overflow-hidden aspect-square relative rotate-45',
+              'border border-amber-400/50',
+              'bg-black',
+            )}
+          ></div>
+        </div>
+        <div
+          className={classNames(
+            'flex items-center px-2',
+            'col-span-9 row-start-1 relative',
+            flip ? 'col-start-1' : 'col-start-4',
+          )}
+        >
+          <div className='w-full border-b border-amber-400/50 relative'></div>
+        </div>
+      </div>
+    )
+  }
+
+  return <div></div>
+  // return (
+  //   <div
+  //     className={classNames(
+  //       'col-span-4 xs:col-span-12 portrait:xm:col-span-4',
+  //       'flex-none grid grid-cols-12',
+  //       'w-full',
+  //       // 'p-2',
+  //       'bg-pink-500',
+  //     )}
+  //   >
+  //     <div
+  //       className={classNames(
+  //         'w-full',
+  //         'aspect-[1/10] xs:aspect-auto portrait:xm:aspect-[1/5]',
+  //         'flex',
+  //         // flip && 'flex-row-reverse',
+  //       )}
+  //     >
+  //       <div
+  //         className={classNames(
+  //           'h-full flex-none aspect-square',
+  //           // 'rotate-45',
+  //           'bg-purple-500',
+  //         )}
+  //       ></div>
+  //     </div>
+  //     {/* <div
+  //       className={classNames(
+  //         'aspect-square portrait:xm:h-full',
+  //         // 'portrait:xm:aspect-[3/1]',
+  //         'col-span-3 row-start-1 portrait:xm:col-span-2',
+  //         flip ? 'col-start-10 portrait:xm:col-start-11' : 'col-start-1',
+  //         'bg-purple-500',
+  //       )}
+  //     ></div> */}
+  //   </div>
+  // )
 }
